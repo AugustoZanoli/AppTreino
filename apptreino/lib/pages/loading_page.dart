38551359b8fb59050bd91d77596ab97d970c301c
Zanoli <zancamar9@gmail.com>
2023-11-08 18:11:28 -0300

    "Implementação da tela de login"

---
+++ apptreino/lib/pages/loading_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          CircularProgressIndicator(
+            backgroundColor: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Carregando...',
+            style: TextStyle(color: Colors.white),
+          )
+        ]),
+      ),
+    );
+  }
+}
